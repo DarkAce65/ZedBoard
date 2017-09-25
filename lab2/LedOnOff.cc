978012a72750914c5d35a4c18cda7aeefbb388c1
DarkAce65 <tvdood99@gmail.com>
2017-09-25 11:04:58 -0400

    Camelcase function calls

--- lab2/LedOnOff.cc
+++ lab2/LedOnOff.cc
@@ -93,20 +93,20 @@ void finalize(char *ptr, int fd) {
  * @param value Value to show on LEDs
  */
 void setLedNumber(char *ptr, int value) {
-	RegisterWrite(ptr, gpio_led1_offset, value % 2);
-	RegisterWrite(ptr, gpio_led2_offset, (value / 2) % 2);
-	RegisterWrite(ptr, gpio_led3_offset, (value / 4) % 2);
-	RegisterWrite(ptr, gpio_led4_offset, (value / 8) % 2);
-	RegisterWrite(ptr, gpio_led5_offset, (value / 16) % 2);
-	RegisterWrite(ptr, gpio_led6_offset, (value / 32) % 2);
-	RegisterWrite(ptr, gpio_led7_offset, (value / 64) % 2);
-	RegisterWrite(ptr, gpio_led8_offset, (value / 128) % 2);
+	registerWrite(ptr, gpio_led1_offset, value % 2);
+	registerWrite(ptr, gpio_led2_offset, (value / 2) % 2);
+	registerWrite(ptr, gpio_led3_offset, (value / 4) % 2);
+	registerWrite(ptr, gpio_led4_offset, (value / 8) % 2);
+	registerWrite(ptr, gpio_led5_offset, (value / 16) % 2);
+	registerWrite(ptr, gpio_led6_offset, (value / 32) % 2);
+	registerWrite(ptr, gpio_led7_offset, (value / 64) % 2);
+	registerWrite(ptr, gpio_led8_offset, (value / 128) % 2);
 }
 
 int main() {
 	// Initialize
 	int fd;
-	char *ptr = Initialize(&fd);
+	char *ptr = initialize(&fd);
 	// Check error
 	if (ptr == MAP_FAILED) {
 		perror("Mapping I/O memory failed - Did you run with 'sudo'?\n");
@@ -116,8 +116,8 @@ int main() {
 	std::cout << "Enter a value less than 256: ";
 	std::cin >> value;
 	// Show the value on the Zedboard LEDs
-	SetLedNumber(ptr, value);
+	setLedNumber(ptr, value);
 	// Done
-	Finalize(ptr, fd);
+	finalize(ptr, fd);
 	return 0;
 }
