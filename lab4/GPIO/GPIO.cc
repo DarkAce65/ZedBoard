a21e6ee2141be8d4fd229f908bbf3a2f07205b60
DarkAce65 <tvdood99@gmail.com>
2017-10-16 10:58:24 -0400

    Format files

--- lab4/GPIO/GPIO.cc
+++ lab4/GPIO/GPIO.cc
@@ -1,50 +1,45 @@
-#include <fcntl.h> 
-#include <stdio.h> 
+#include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <iostream>
 #include <stdlib.h>
 #include "GPIO.h"
- 
-GPIO::GPIO(int number) 
-{ 
-	// GPIO device files will follow the format 
+
+GPIO::GPIO(int number) {
+	// GPIO device files will follow the format
 	//
-	//sys/class/gpio/gpio<NR>/value 
-	// <NR> has to be replaced with the actual device number passed as an 
-	// argument to the class constructor. 
-	char device_name[128]; 
+	//sys/class/gpio/gpio<NR>/value
+	// <NR> has to be replaced with the actual device number passed as an
+	// argument to the class constructor.
+	char device_name[128];
 
-	sprintf(device_name, "/sys/class/gpio/gpio%d/value", number); 
+	sprintf(device_name, "/sys/class/gpio/gpio%d/value", number);
 
-	// Open special device file and store file descriptor in class member. 
+	// Open special device file and store file descriptor in class member.
 	fd = open(device_name, O_WRONLY);
 
-	if (fd < 0)
-	{
-	std::cerr << "Cannot open " << device_name <<
-	" - forgot sudo?  \n";
-	exit(1);
+	if (fd < 0) {
+		std::cerr << "Cannot open " << device_name <<
+		" - forgot sudo?  \n";
+		exit(1);
 	}
-} 
+}
 
-GPIO::~GPIO() 
-{ 
-	// Close open file descriptor 
-	close(fd); 
-} 
+GPIO::~GPIO() {
+	// Close open file descriptor
+	close(fd);
+}
 
-void GPIO::GeneratePWM(int period, int pulse, int num_periods) 
-{ 
-	// Generate num_perios of the PWM signal 
-	for (int i = 0; i < num_periods; i++) 
-	{ 
-		// Write ASCII character "1" to raise pin to 1, starting the 
-		// ON cycle, then wait duration of pulse. 
-		write(fd, "1", 1); 
-		usleep(pulse); 
-		// Write ASCII character "0" to lower pin to 0, starting the 
-		// OFF cycle, then wait the rest of the period time. 
-		write(fd, "0", 1); 
-		usleep(period - pulse); 
-	} 
+void GPIO::GeneratePWM(int period, int pulse, int num_periods) {
+	// Generate num_perios of the PWM signal
+	for (int i = 0; i < num_periods; i++) {
+		// Write ASCII character "1" to raise pin to 1, starting the
+		// ON cycle, then wait duration of pulse.
+		write(fd, "1", 1);
+		usleep(pulse);
+		// Write ASCII character "0" to lower pin to 0, starting the
+		// OFF cycle, then wait the rest of the period time.
+		write(fd, "0", 1);
+		usleep(period - pulse);
+	}
 }
--- lab4/GPIO/main.cc
+++ lab4/GPIO/main.cc
@@ -1,11 +1,11 @@
-#include "GPIO.h" 
-int main() 
-{ 
-// Open device file 13 on Linux file system 
-GPIO gpio(13); 
-// Generate PWM signal with 20ms period and 1.5ms on time. 
-// Generate 400 periods, this will take 20ms * 400 iterations = 8s 
-gpio.GeneratePWM(20000, 1500, 400); 
-// Done 
-return 0; 
+#include "GPIO.h"
+
+int main() {
+	// Open device file 13 on Linux file system
+	GPIO gpio(13);
+	// Generate PWM signal with 20ms period and 1.5ms on time.
+	// Generate 400 periods, this will take 20ms * 400 iterations = 8s
+	gpio.GeneratePWM(20000, 1500, 400);
+	// Done
+	return 0;
 }
