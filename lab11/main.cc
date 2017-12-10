7658cc70cc11aef141dd66d14fd0627924690747
DarkAce65 <tvdood99@gmail.com>
2017-12-10 02:31:40 -0500

    Update main.cc with ZedBoard code

--- lab11/main.cc
+++ lab11/main.cc
@@ -4,13 +4,13 @@
 int main() {
     RoboticArm robotic_arm;
 
-    while(true) {
-        robotic_arm.MoveServo(4, 45, 45);
-        robotic_arm.MoveServo(3, 45, 45);
-        sleep(1);
+	robotic_arm.MoveServo(1, 35, 30);
+	sleep(6);
+	robotic_arm.MoveServo(4, 0, 45);
+	sleep(1);
 
-        robotic_arm.MoveServo(4, 90, 45);
-        robotic_arm.MoveServo(3, 90, 45);
-        sleep(1);
-    }
+	robotic_arm.MoveServo(1, 135, 200);
+	robotic_arm.MoveServo(2, 165, 150);
+	usleep(500000);
+	robotic_arm.MoveServo(4, 100, 255);
 }
